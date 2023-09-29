df92f3534d9c4ee34914c75362949853ff80060d
Bea Pan <bpankowska@gmail.com>
2023-09-29 16:33:14 +0300

    added background images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 
+import React from "react";
+
+const api = {
+  key: "0c1fdc1979efc1a7313bfc905e40e625",
+  base: "https://api.openweathermap.org/data/2.5/"
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-    hello
+    <div className="app warm">
+    <main>
+      <div className="search-box">
+        <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        ></input>
+      </div>
+    </main>
     </div>
   );
 }
